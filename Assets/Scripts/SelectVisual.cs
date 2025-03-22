03802d310d2ac4e90b330a9bee7e95ffd392dae4
Kenneth <kenships0402@gmail.com>
2025-03-22 14:58:16 -0400

    Merge remote-tracking branch 'origin/Develop' into Develop
    Spray can visual and grabbable

---
+++ Assets/Scripts/SelectVisual.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class SelectVisual : MonoBehaviour
+{
+    [SerializeField] private XRGrabInteractable grabObject;
+    [SerializeField] private GameObject selectVisual;
+    [SerializeField] private GameObject activateVisual;
+    private void Start()
+    {
+        grabObject.hoverEntered.AddListener(OnHoverEnter);
+        grabObject.hoverExited.AddListener(OnHoverExit);
+        grabObject.selectEntered.AddListener(OnHoverExit);
+        grabObject.activated.AddListener(OnActivate);
+        grabObject.deactivated.AddListener(OnDeactivate);
+        grabObject.selectExited.AddListener(OnDeactivate);
+    }
+
+    private void OnDeactivate(SelectExitEventArgs arg0)
+    {
+        Deactivate();
+    }
+
+    private void OnDeactivate(DeactivateEventArgs arg0)
+    {
+        Deactivate();
+    }
+
+    private void OnActivate(ActivateEventArgs arg0)
+    {
+        Activate();
+    }
+
+    private void OnHoverExit(SelectEnterEventArgs arg0)
+    {
+        Hide();
+    }
+
+    private void OnHoverExit(HoverExitEventArgs arg0)
+    {
+        Hide();
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs arg0)
+    {
+        if (!grabObject.isSelected)
+        {
+            Show();
+        }
+    }
+
+    private void Show()
+    {
+        selectVisual.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        selectVisual.SetActive(false);
+    }
+
+    private void Activate()
+    {
+        activateVisual.SetActive(true);
+    }
+
+    private void Deactivate()
+    {
+        activateVisual.SetActive(false);
+    }
+}
